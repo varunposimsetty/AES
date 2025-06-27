4c0abb6e5f2572e2a905410f9817464990ac1cfa
Varun Posimsetty <VarunSrinivas.Posimsetty@infineon.com>
2025-06-27 16:10:59 +0200

    present status

--- AES/sim/tb_sub_bytes.vhd
+++ AES/sim/tb_sub_bytes.vhd
@@ -6,6 +6,9 @@ entity tb is
 end entity tb;
 
 architecture bhv of tb is 
+    signal clk : std_ulogic := '0';
+    signal rst : std_ulogic := '0';
+    signal start : std_ulogic := '0';
     signal data_in : std_ulogic_vector(127 downto 0) := (others => '0');
     signal key_in : std_ulogic_vector(127 downto 0) := (others => '0');
     signal data_out : std_ulogic_vector(127 downto 0);
@@ -13,6 +16,9 @@ architecture bhv of tb is
     begin 
     DUT_AES : entity work.AES(RTL)
         port map(
+            i_clk => clk,
+            i_nrst_async => rst,
+            i_start => start,
             i_data_in => data_in,
             i_cipher_key => key_in,
             o_data_out => data_out
@@ -21,16 +27,24 @@ architecture bhv of tb is
     proc_tb : process is 
     begin 
         wait for 10 ns;
+        rst <= '1';
+        start <= '1';
         data_in <= x"1A4567F310245543210A135667900100";
         key_in <= x"A1B2C3D4E5F60718293A4B5C6D7E8F90";
-        wait for 20 ns;
+        wait for 125 ns;
         data_in <= x"FEDCBA9876543210FEDCBA9876543210";
         key_in <= x"1A4567F310245543210A135667900100";
-        wait for 30 ns;
+        wait for 130 ns;
         data_in <= x"A1B2C3D4E5F60718293A4B5C6D7E8F90";
         key_in <= x"FEDCBA9876543210FEDCBA9876543210";
-        wait for 20 ns;
+        wait for 120 ns;
         wait;
     end process proc_tb;
+
+    proc_clk : process is
+        begin 
+        wait for 5 ns;
+        clk <= not clk;
+    end process;
 
 end architecture bhv;
--- AES/src/unitAES.vhd
+++ AES/src/unitAES.vhd
@@ -19,53 +19,16 @@ entity AES is
 end entity AES;
 
 architecture RTL of AES is
-begin 
-
-    SBOX : entity work.sbox(RTL)
-            port map(
-                i_byte_in => byte_in,
-                o_byte_out => byte_out
-        );
-        
-        SHIFT_ROWS : entity work.shift_rows(RTL)
-            port map(
-               i_row_state_in => row_state_in,
-               o_row_state_out => row_state_out
-        );
-
-        MOVE_COLUMNS : entity work.move_columns(RTL)
-            port map(
-                i_column_state_in => column_state_in,
-                o_column_state_out => column_state_out
-        );
-
-        KEY_EXPANSION : entity work.key_expansion(RTL)
-            generic map(
-                key_size => KEY_SIZE,
-                word_size => TEXT_SIZE,
-                rounds => ROUNDS
-
-            )
-            port map(
-                i_key => i_cipher_key,
-                o_expanded_key => expanded_key
-        );
-
-        ADD_ROUND_KEY : entity work.addRoundKey(RTL)
-            port map(
-                i_state_in => round_key_state_in,
-                i_expanded_key => round_expanded_key,
-                o_state_out => round_key_state_out
-        );
-
-    process(i_clk,i_nrst_async) is 
-        variable count : integer := 0;
-    begin 
-        if(i_nrst_async = '0') then 
-
-        elsif(rising_edge(i_clk)) then
-            case count is 
-                when 0 => 
-                     
-
-            
+    signal byte_in : std_ulogic_vector(127 downto 0) := (others => '0');
+    signal byte_out : std_ulogic_vector(127 downto 0);
+    signal row_state_in : std_ulogic_vector(127 downto 0) := (others => '0');
+    signal row_state_out : std_ulogic_vector(127 downto 0);
+    signal column_state_in : std_ulogic_vector(127 downto 0) := (others => '0');
+    signal column_state_out : std_ulogic_vector(127 downto 0);
+    signal expanded_key : std_ulogic_vector(((rounds+1)*128) - 1 downto 0);
+    signal round_key_state_in : std_ulogic_vector(127 downto 0) := (others => '0');
+    signal round_expanded_key : std_ulogic_vector(127 downto 0) := (others => '0');
+    signal round_key_state_out : std_ulogic_vector(127 downto 0);
+    --
+    type tExpandedKeyBank is array(0 to rounds) of std_ulogic_vector(127 downto 0);
+    signal expanded_key_bank : tExpandedKeyBank := (others => (others => '0'));
